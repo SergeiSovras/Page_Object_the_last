a09f9658acdbdcc45bc99be0e47531cc4c55d4a7
Siarhei.Sauras <sergei.sovras@gmail.com>
2022-04-04 12:09:58 +0300

    4.2 Creating assert for error handling

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,17 @@
+from selenium.common.exceptions import NoSuchElementException
 class BasePage():
 
-    def __init__(self, browser, url):                #конструктор
+    def __init__(self, browser, url, timeout=10):                #конструктор
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):                                  #открывает страницу
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
